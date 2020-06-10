f58e0d30f815b370fe7fbeb09d32781a4c7defda
Matthew M <mbmoorin@gmail.com>
2020-06-10 17:06:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-animal = 'dog'
+var animal = 'dog'
 
 function myAnimal() {
   // You should not need to modify this function
@@ -11,7 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  var animal = cat
+  animal = cat
   return animal
 }
 
